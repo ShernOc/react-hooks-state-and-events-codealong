e265183aeac49cc5d500ca98455530513ef17cdd
Sherlyne <sherlynea1994@hotmail.com>
2024-08-11 14:01:02 +0300

    usestate and event changed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
+// The button should say on of upon click 
 function Toggle() {
-  return <button>OFF</button>;
+  // initialize the state which returns an array
+  const [isOn, setIsOn]= useState(false);
+
+// event function 
+  function handleClick(){
+    // we are calling the setfuntion to update the state 
+    setIsOn((isOn)=>!isOn); 
+  }
+
+  // we can also add the button background to be red and use the same state is the button is on it turns red. 
+
+   const color = isOn ? "red" : "white";
+
+  return <button style={{background:color}} onClick={handleClick}>{isOn?"ON":"OFF"} </button>;
 }
 
 export default Toggle;
